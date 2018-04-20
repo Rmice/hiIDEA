5f51da6936f5e396b9f19168dc27a2c7c58dfa28
Rmice <1191067121@qq.com>
2018-04-21 05:10:40 +0800

    test

---
+++ src/HiIDEA.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: HiIDEA
+ * Author:   Administrator
+ * Date:     2018-04-21 5:05
+ * Description: 牛逼git
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ * <p>
+ * package PACKAGE_NAME;
+ * /**
+ * 〈一句话功能简述〉<br>
+ * 〈牛逼git〉
+ *
+ * @author Administrator
+ * @create 2018-04-21
+ * @since 1.0.0
+ */
+public class HiIDEA {
+    public static void main(String srgs[])  {
+        System.out.println("Hi IDEA");
+    }
+}
